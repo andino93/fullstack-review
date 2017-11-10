011465a6206d017d92a29130f9c3cfbef31a59b7
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-09 16:45:31 -0800

    add bungle files to git

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -24,7 +24,10 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.term} onChange={this.onChange.bind(this)}/>
+      Enter a github username: <input 
+        value={this.state.term}
+        onChange={this.onChange.bind(this)}
+      />
       <button onClick={this.search.bind(this)}> Add Repos </button>
     </div>)
   }
